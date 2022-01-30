24ecb6f734b246660530d29626f88dcb60f68d0b
davidwobido <davidwobido@googlemail.com>
2022-01-30 11:41:18 +0100

    :sparkles: Add Example image

--- src/Components/ProjectCard/ProjectCard.stories.tsx
+++ src/Components/ProjectCard/ProjectCard.stories.tsx
@@ -1,5 +1,6 @@
 import ProjectCard from "./ProjectCard";
 import LogoFestivalFilter from "../../../lib/Logos/FF_Logo_small.svg";
+import ProjectImageFF from "../../../lib/Images/FF_Example_Screenshot.png";
 
 export default {
   component: ProjectCard,
@@ -17,6 +18,7 @@ function. The detail pages present information such as e.g. the entire
 line-up, location or price.
 "
     gitLink="https://github.com/davidwobido/festival-filter"
-    projectImage=""
+    projectLink="https://festival-filter.herokuapp.com/"
+    projectImage={ProjectImageFF}
   />
 );
