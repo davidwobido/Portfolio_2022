a2edd4a7b405e71b500ec0e030d8263ddb63a0a8
davidwobido <davidwobido@googlemail.com>
2022-01-30 08:42:33 +0100

    :bug: Change styling and add links

--- src/Components/ContactBar/ContactBar.tsx
+++ src/Components/ContactBar/ContactBar.tsx
@@ -9,15 +9,25 @@ import Xing from "../../../lib/Icons/Icon_Xing.svg";
 export default function ContactBar() {
   return (
     <div className={styles.bar}>
-      <img src={GitHub} className={styles["contact-icon"]} alt="GitHub"></img>
-      <img
-        src={LinkedIn}
-        className={styles["contact-icon"]}
-        alt="LinkedIn"
-      ></img>
-      <img src={Xing} className={styles["contact-icon"]} alt="Xing"></img>
-      <img src={Mail} className={styles["contact-icon"]} alt="Mail"></img>
-      <img src={Lastfm} className={styles["contact-icon"]} alt="LastFM"></img>
+      <a href="https://github.com/davidwobido/">
+        <img src={GitHub} className={styles["contact-icon"]} alt="GitHub"></img>{" "}
+      </a>
+      <a href="https://www.linkedin.com/in/davidwobido/">
+        <img
+          src={LinkedIn}
+          className={styles["contact-icon"]}
+          alt="LinkedIn"
+        ></img>
+      </a>
+      <a href="https://www.xing.com/profile/David_Wobido/cv">
+        <img src={Xing} className={styles["contact-icon"]} alt="Xing"></img>
+      </a>
+      <a href="mailto:davidwobido@googlemail.com">
+        <img src={Mail} className={styles["contact-icon"]} alt="Mail"></img>
+      </a>
+      <a href="https://www.last.fm/de/user/wobe_teh_Onsk">
+        <img src={Lastfm} className={styles["contact-icon"]} alt="LastFM"></img>
+      </a>
     </div>
   );
 }
