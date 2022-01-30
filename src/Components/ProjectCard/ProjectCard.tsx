3dafa223f587507052708af52c52dab8e73e9210
davidwobido <davidwobido@googlemail.com>
2022-01-30 11:41:36 +0100

    :lipstick: Style card

--- src/Components/ProjectCard/ProjectCard.tsx
+++ src/Components/ProjectCard/ProjectCard.tsx
@@ -1,30 +1,42 @@
 import styles from "./ProjectCard.module.css";
 import "../../globals.css";
 import IconGitHub from "../../../lib/Icons/Icon_GitHub.svg";
+import IconExternal from "../../../lib/Icons/Icon_External.svg";
 import { ProjectCardTypes } from "../../../types";
 
 export default function ProjectCard({
   logo,
   headline,
   description,
-  gitLink,
   projectImage,
+  gitLink,
+  projectLink,
 }: ProjectCardTypes) {
   return (
     <div className={styles.card}>
-      <div className={styles.text}>
+      <div className={styles["column-text"]}>
         <img className={styles.logo} src={logo} alt="" />
         <h3 className={styles.headline}>{headline}</h3>
         <p className={styles.copy}>{description}</p>
-        <div className={styles.links}>
-          <a href={gitLink}>
+        <div className={styles["links-wrapper"]}>
+          <a href={gitLink} className={styles.links}>
             <img src={IconGitHub} alt="Logo GitHub" />
-            <div className={styles.link}>Link to Github</div>
+            <span className={styles.link}>Link to Github</span>
           </a>
+          {projectLink && (
+            <a href={projectLink} className={styles.links}>
+              <img src={IconExternal} alt="" />
+              <span className={styles.link}>Link to Github {headline}</span>
+            </a>
+          )}
         </div>
       </div>
-      <div className={styles.image}>
-        <img src={projectImage} alt={`Project image of ${headline}`} />
+      <div className={styles["column-image"]}>
+        <img
+          className={styles["project-image"]}
+          src={projectImage}
+          alt={`Project image of ${headline}`}
+        />
       </div>
     </div>
   );
