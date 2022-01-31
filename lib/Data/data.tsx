b0bdff7e80c7016df27d589c02056b3ce3ff1559
davidwobido <davidwobido@googlemail.com>
2022-01-31 18:36:41 +0100

    :monocle_face: Change and finalize "CV" in data.tsx

--- lib/Data/data.tsx
+++ lib/Data/data.tsx
@@ -2,21 +2,61 @@ export const Jobs = [
   {
     company: "Helder",
     location: "Berlin",
-    start: "03/2019",
-    end: "08/2021",
+    start: "04/’19",
+    end: "08/’21",
     position: "(Senior) Brand Designer",
     tasks:
-      "Verantwortlich für die Entwicklung von Gestaltungskonzepten, deren Ausführung und die Koordination mit Produzent:innen und Entwickler:innen <br> Leaddesign einzelner Projekte sowie Ansprechpartner einzelner Kund:innen <br> Gestaltung von Brand Designs (Logo, Typografie, Farbkonzept, Bild- & Iconsprache), Websites, Magazinen, Animationen ",
-    clients: "Kunden: StartUps, DCSO, BZgA, ASC, MSC",
+      "Concept, Design, Completion of Brand Design (Logo, Typography, Color Concept, Image & Icon Language), Web Design, Editorial Design and Animations",
+    clients: "StartUps, DCSO, BZgA, ASC, MSC",
   },
   {
     company: "Freelancer",
     location: "Berlin & Mainz",
-    start: "07/2018",
-    end: "12/2020",
+    start: "07/’18",
+    end: "12/’20",
     position: "Graphic Designer & Art Director",
     tasks:
-      "Verantwortlich für alles was im Daily Business anfiel <br> Konzeption, Entwurf, Gestaltung, Umsetzung von Corporate-, Grafik- und Retail-Design-Projekten <br> Umsetzung kleinerer Websites in Word-Press",
+      "Concept, Design, Completion of Brand Design (Logo, Typography, Color Concept, Image & Icon Language), Web Design, Editorial Design. Building some small websites",
     clients: "New Balance, Nike, Haglöfs, EWE, Cocomore",
+  },
+  {
+    company: "martin & friends",
+    location: "Mainz",
+    start: "03/’16",
+    end: "07/’18",
+    position: "Graphic & Retail Designer",
+    tasks:
+      "Design, Completion of Graphic Design and Retail Design Projects (Shop-in-Shop-Systems, Retail-Fair-Systems, Banners, Windows and Magazines)",
+    clients: "New Balance, Haglöfs",
+  },
+  {
+    company: "Pioneers",
+    location: "Mainz",
+    start: "03/’15",
+    end: "03/’16",
+    position: "Graphic Designer",
+    tasks:
+      "Design, Completion of Graphic Design and Retail Design Projects (Shop-in-Shop-Systems, Retail-Fair-Systems, Banner, Windows and Presentations)",
+    clients: "Nike",
+  },
+  {
+    company: "Primes Research",
+    location: "Mainz",
+    start: "11/’13",
+    end: "03/’15",
+    position: "Researcher",
+    tasks:
+      "Browse through all kinds of magazines for specific keywords to provide newsletters for the clients",
+    clients: "Daimler, VW, Toyota, Renault, Jaguar",
+  },
+  {
+    company: "FBO",
+    location: "Saarbrücken",
+    start: "07/’10",
+    end: "03/’13",
+    position: "Training Media Designer",
+    tasks:
+      "Completion of various print products, such as magazines, posters, flyers (Typesetting, Image Retouching) ",
+    clients: "SaarLB, Ursapharm",
   },
 ];
