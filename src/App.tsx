b748f30c0a46b8e16e7639efc25843075854972d
davidwobido <davidwobido@googlemail.com>
2022-02-03 09:16:30 +0100

    :heavy_plus_sign: Install AOS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+// import "./globals.css";
 import styles from "./App.module.css";
 import Accordion from "./Components/Accordion/Accordion";
 import ContactBar from "./Components/ContactBar/ContactBar";
