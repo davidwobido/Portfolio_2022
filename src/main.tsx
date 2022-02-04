b7fe7be3d059dae731897075ddeb23f7c784c3b1
davidwobido <davidwobido@googlemail.com>
2022-02-04 10:05:13 +0100

    :bug: Delete Impressum from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./globals.css";
 import App from "./App";
-import Impressum from "./Pages/Impressum/Impressum";
 
 ReactDOM.render(
   <React.StrictMode>
