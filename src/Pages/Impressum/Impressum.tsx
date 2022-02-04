8e65a77fe8b42c995ae0b0fd9c34265efa399701
davidwobido <davidwobido@googlemail.com>
2022-02-04 08:47:59 +0100

    :tada: Start impressum/legal notice page

---
+++ src/Pages/Impressum/Impressum.tsx
@@ -0,0 +1,18 @@
+import styles from "./Impressum.module.css";
+
+function Impressum() {
+  return (
+    <>
+      <h1 className={styles.title}>Impressum</h1>
+      <div>
+        <h2>EN</h2>
+      </div>
+
+      <div>
+        <h2>DE</h2>
+      </div>
+    </>
+  );
+}
+
+export default Impressum;
