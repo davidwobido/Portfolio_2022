c41073c766efdf01bab3ed499785d62cc6060654
davidwobido <davidwobido@googlemail.com>
2022-02-04 14:13:22 +0100

    :bug: Add ScrollToTop.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import "./globals.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./Components/ScrollToTop/scrollToTop";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
